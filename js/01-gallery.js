8f2ba5cf6fcce1f3c41cc895f33a535bd4fb22f7
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-10-27 13:46:08 +0200

    task01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+ "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+
+ const list = document.querySelector('.gallery');
+
+const markup = galleryItems
+.map(item => 
+    `<ul>
+<li>
+<img class="picture" src=${item.preview} width = '200' alt='${item.description}'></img>
+</li>
+</ul>`)
+    .join('');
+
+    list.insertAdjacentHTML('beforeend', markup);
+
+
+const pictures = document.querySelectorAll('.picture');
+
+list.style.display = 'flex';
+list.style.flexWrap = 'column-reverse';
+list.style.listStyle = 'none';
+list.style.gap = '20px';
+list.style.justifyContent = 'center';
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,24 +2,3 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-
-const list = document.querySelector('.gallery');
-
-const markup = galleryItems
-.map(item => 
-    `<ul>
-<li>
-<img class="picture" src=${item.preview} width = '200' alt='${item.description}'></img>
-</li>
-</ul>`)
-    .join('');
-    list.insertAdjacentHTML('beforeend', markup);
-
-
-const pictures = document.querySelectorAll('.picture');
-
-list.style.display = 'flex';
-list.style.flexWrap = 'column-reverse';
-list.style.listStyle = 'none';
-list.style.gap = '20px';
-list.style.justifyContent = 'center';
