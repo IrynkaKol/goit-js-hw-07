e9e194f0e7015ba98feb27785fb6b088b70ddb23
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-10-28 23:43:24 +0200

    task01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,14 +5,17 @@ console.log(galleryItems);
  "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
 
  const galllery = document.querySelector('.gallery');
+
  galllery.addEventListener('click', onClick);
 
  function onClick(evt) {
+    //evt.preventDefault();
     if (evt.target.nodeName !== 'IMG') {
         return;
     }
-    console.log(evt.target)
+    console.log(evt.target)  // має відкриватись можальне вікно з картинкою
 }
+
  
 const markup = galleryItems
 .map(item => 
@@ -20,6 +23,7 @@ const markup = galleryItems
 <a class = "galllery__link" href = "${item.original}">
 <img class="gallery__image"
 src="${item.preview}"
+data-source="${item.original}"
 alt='${item.description}'>
 </img>
 </a>
