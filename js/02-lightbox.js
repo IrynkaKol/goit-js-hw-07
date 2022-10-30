f0ae8c1344313c5d4ab775a98d531fbc51a2a755
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-10-30 12:11:58 +0100

    task-02done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+.map(({preview, original, description}) =>
+`<a class="gallery__item" href="${original}">
+<img  class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+).join('');
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captions: true,
+    captionDelay: 250,
+    captionSelector: 'img',
+    captionPosition: 'button'
+        
+    }) //https://simplelightbox.com/
